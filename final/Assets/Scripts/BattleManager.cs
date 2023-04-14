7889d56f6395f4b190fc4dce2334994ab8d6765c
Kristen <kristenmarcinek@protonmail.com>
2023-04-13 22:06:02 -0400

    add base initiative

--- final/Assets/Scripts/BattleManager.cs
+++ final/Assets/Scripts/BattleManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
+    public GameManager gm;
+
+    public float initiativeCheck = 0;
+
     // Start is called before the first frame update
     void Start()
     {
--- final/Assets/Scripts/GameManager.cs
+++ final/Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
+//using System.Diagnostics;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float initiativeNumber = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,11 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Initiative()
+    {
+        initiativeNumber = Random.Range(1, 20);
+        Debug.Log(initiativeNumber);
     }
 }
