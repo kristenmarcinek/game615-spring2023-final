34af122cd4894e1a8f9175f1ef81ad3f4fd4f80d
lemosadam <117216643+lemosadam@users.noreply.github.com>
2023-04-26 13:09:58 -0400

    Finished Hit Check

--- final/Assets/Scripts/BattleManager.cs
+++ final/Assets/Scripts/BattleManager.cs
@@ -10,27 +10,56 @@ public class BattleManager : MonoBehaviour
 
     public List<int> initiatives = new List<int>();
 
+    public int hitBase;
+    public int turnPlayerHitMod;
+    public int selectedEnemyDodge;
+    public int hitChance;
+    public int hitRoll;
+    public bool attackHits;
+
     //public float initiativeCheck = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        hitBase = 50;
+        turnPlayerHitMod = 40;
+        selectedEnemyDodge = 20;
+        attackHits = false;
+
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
         foreach (GameObject enemy in enemies) {
             initiatives.Add(enemy.GetComponent<EnemyController>().enemyInitiativeNumber);
             print(initiatives[1]);
+
+            
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown ("space"))
+        {
+            ToHit();
+        }
     }
 
     public void ToHit() {
+        hitChance = hitBase + turnPlayerHitMod - selectedEnemyDodge;
+        hitRoll = Random.Range(1, 100);
+        if (hitRoll <= hitChance)
+            {
+                attackHits = true;
+            }
+        else
+            {
+            attackHits = false;
+            }
         
+        Debug.Log(attackHits);
     }
 
     public void EnemyAttack() {
